e40bd59253cebc752eccb440cfcb83b518b3a946
Amy Dang <ndangamy@gmail.com>
2025-01-27 10:41:18 -0800

    update layout + add icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import SettingsIcon from '$lib/components/icons/SettingsIcon.svelte';
 	import SpeakerWaveIcon from '$lib/components/icons/SpeakerWaveIcon.svelte';
-	import metronomeClick from '$lib/assets/metronome-85688.mp3';
+	import PlayIcon from '$lib/components/icons/PlayIcon.svelte';
+	import PlusIcon from '$lib/components/icons/PlusIcon.svelte';
+	import MinusIcon from '$lib/components/icons/MinusIcon.svelte';
 </script>
 
 <main class="flex h-screen w-screen justify-center bg-red-400">
@@ -11,21 +13,28 @@
 			<SpeakerWaveIcon class="size-10 text-red-200" />
 		</header>
 
-		<div class="flex h-4/5 content-center justify-center">
+		<div class="flex h-4/5 flex-col content-center justify-center gap-4">
 			<div class="grid grid-cols-4 grid-rows-1 gap-16 place-self-center">
 				<div class="h-32 w-32 bg-teal-400"></div>
 				<div class="h-32 w-32 bg-teal-400"></div>
 				<div class="h-32 w-32 bg-teal-400"></div>
 				<div class="h-32 w-32 bg-teal-400"></div>
 			</div>
+			<div class="mt-8 flex w-full justify-center">
+				<PlayIcon class="size-8 place-self-center text-white" />
+			</div>
 		</div>
 		<footer class="flex grow-0 flex-row place-content-center justify-between">
-			<SettingsIcon class="size-10 text-white" />
+			<SettingsIcon class="size-8 text-white" />
 			<div class="align-center flex flex-row justify-center gap-4 text-3xl text-white">
 				<div class="grid grid-rows-2 gap-0 leading-6">
 					<span>4</span><span>4</span>
 				</div>
-				<div class="self-center text-5xl underline decoration-2 underline-offset-8">123</div>
+				<span class="self-center text-5xl underline decoration-2 underline-offset-8">123</span>
+				<div class="align-center relative top-2 flex flex-col gap-2">
+					<button class="p-0"><PlusIcon class="size-5" /></button>
+					<button class="p-0"><MinusIcon class="size-5" /></button>
+				</div>
 			</div>
 		</footer>
 	</div>
