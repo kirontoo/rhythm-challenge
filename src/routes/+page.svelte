9f5793486e09ebe81bc267a4a2d691ccccec6c48
Amy Dang <ndangamy@gmail.com>
2025-02-02 17:45:02 -0800

    replace icons with buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import SettingsIcon from '$lib/components/icons/SettingsIcon.svelte';
 	import SpeakerWaveIcon from '$lib/components/icons/SpeakerWaveIcon.svelte';
-	import PlayIcon from '$lib/components/icons/PlayIcon.svelte';
-	import PlusIcon from '$lib/components/icons/PlusIcon.svelte';
-	import MinusIcon from '$lib/components/icons/MinusIcon.svelte';
+	import SixteenthNoteTile from '$lib/components/tiles/SixteenthNoteTile.svelte';
+	import QuarterNoteTile from '$lib/components/tiles/QuarterNoteTile.svelte';
+	import QuarterRestTile from '$lib/components/tiles/QuarterRestTile.svelte';
+	import EighthNoteTile from '$lib/components/tiles/EighthNoteTile.svelte';
+	import TripletTile from '$lib/components/tiles/TripletTile.svelte';
+	import MediaControlsButton from '$lib/components/buttons/MediaControlsButton.svelte';
+	import TempoButton from '$lib/components/buttons/TempoButton.svelte';
+
+	let tempo = $state(100);
 </script>
 
-<main class="flex h-screen w-screen justify-center bg-red-400">
+<main class="flex h-screen w-screen justify-center bg-purple-400">
 	<div class="container flex flex-col content-center justify-center">
 		<header class="flex grow-0 flex-row place-content-center justify-between">
 			<div></div>
@@ -15,26 +21,23 @@
 
 		<div class="flex h-4/5 flex-col content-center justify-center gap-4">
 			<div class="grid grid-cols-4 grid-rows-1 gap-16 place-self-center">
-				<div class="h-32 w-32 bg-teal-400"></div>
-				<div class="h-32 w-32 bg-teal-400"></div>
-				<div class="h-32 w-32 bg-teal-400"></div>
-				<div class="h-32 w-32 bg-teal-400"></div>
+				<QuarterNoteTile />
+				<QuarterRestTile />
+				<EighthNoteTile />
+				<SixteenthNoteTile />
+				<TripletTile />
 			</div>
 			<div class="mt-8 flex w-full justify-center">
-				<PlayIcon class="size-8 place-self-center text-white" />
+				<MediaControlsButton class="size-8 text-white" />
 			</div>
 		</div>
 		<footer class="flex grow-0 flex-row place-content-center justify-between">
 			<SettingsIcon class="size-8 text-white" />
-			<div class="align-center flex flex-row justify-center gap-4 text-3xl text-white">
-				<div class="grid grid-rows-2 gap-0 leading-6">
+			<div class="align-center flex flex-row justify-center gap-2 text-3xl text-white">
+				<div class="relative top-3 grid grid-rows-2 gap-0 leading-6">
 					<span>4</span><span>4</span>
 				</div>
-				<span class="self-center text-5xl underline decoration-2 underline-offset-8">123</span>
-				<div class="align-center relative top-2 flex flex-col gap-2">
-					<button class="p-0"><PlusIcon class="size-5" /></button>
-					<button class="p-0"><MinusIcon class="size-5" /></button>
-				</div>
+				<TempoButton bind:tempo />
 			</div>
 		</footer>
 	</div>
