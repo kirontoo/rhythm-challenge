d02381aeb2f9ae7515897fa0d3882a9c5fbcaca9
Amy Dang <ndangamy@gmail.com>
2025-02-12 16:17:56 -0800

    set up metronome and draw boxes on canvs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,61 @@
 <script lang="ts">
 	import SettingsIcon from '$lib/components/icons/SettingsIcon.svelte';
-	import SpeakerWaveIcon from '$lib/components/icons/SpeakerWaveIcon.svelte';
-	import SixteenthNoteTile from '$lib/components/tiles/SixteenthNoteTile.svelte';
-	import QuarterNoteTile from '$lib/components/tiles/QuarterNoteTile.svelte';
-	import QuarterRestTile from '$lib/components/tiles/QuarterRestTile.svelte';
-	import EighthNoteTile from '$lib/components/tiles/EighthNoteTile.svelte';
-	import TripletTile from '$lib/components/tiles/TripletTile.svelte';
 	import MediaControlsButton from '$lib/components/buttons/MediaControlsButton.svelte';
 	import TempoButton from '$lib/components/buttons/TempoButton.svelte';
+	import Metronome from '$lib/metronome.svelte';
+	import { onMount } from 'svelte';
+	import { Rhythm } from '$lib/metronome.svelte';
 
-	let tempo = $state(100);
+	let metronome: Metronome | null = null;
+	let canvasCtx: CanvasRenderingContext2D;
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		canvasCtx = canvas.getContext('2d')
+
+		if (!metronome) {
+			metronome = new Metronome(window, draw);
+			metronome.noteResolution = Rhythm.QuarterNote;
+		}
+	});
+
+	function draw(currentNote: number) {
+		var x = Math.floor( canvas.width / 8 );
+		canvasCtx.clearRect(0,0,canvas.width, canvas.height); 
+		let firstBeat = currentNote / 4;
+		for (var i = 0; i < 4; i++) {
+				if (i == firstBeat) {
+					// draw a pink box only on the 1st beat of each measure
+					canvasCtx.fillStyle = "oklch(0.718 0.202 349.761)";
+				} else {
+					// draw teal box
+					canvasCtx.fillStyle = "oklch(0.777 0.152 181.912)";
+				}
+
+				canvasCtx.fillRect( x * (i+1) + (canvas.width / 16), x, 150, 150);
+		}
+		canvasCtx.font = "40px serif";
+		canvasCtx.fillStyle = "white";
+		canvasCtx.fillText(`Current beat: ${currentNote + 1}`, 10, 50);
+	}
+
+	function onPause() {
+		metronome.play();
+	}
+
+	function onPlay() {
+		metronome.play();
+	}
 </script>
 
-<main class="flex h-screen w-screen justify-center bg-purple-400">
-	<div class="container flex flex-col content-center justify-center">
-		<header class="flex grow-0 flex-row place-content-center justify-between">
-			<div></div>
-			<SpeakerWaveIcon class="size-10 text-red-200" />
-		</header>
-
+<main class="flex h-screen w-screen justify-center bg-slate-400">
+	<div class="container flex flex-col content-center justify-center relative">
 		<div class="flex h-4/5 flex-col content-center justify-center gap-4">
 			<div class="grid grid-cols-4 grid-rows-1 gap-16 place-self-center">
-				<QuarterNoteTile />
-				<QuarterRestTile />
-				<EighthNoteTile />
-				<SixteenthNoteTile />
-				<TripletTile />
+				<canvas height={400} width={1600} bind:this={canvas}></canvas>
 			</div>
 			<div class="mt-8 flex w-full justify-center">
-				<MediaControlsButton class="size-8 text-white" />
+				<MediaControlsButton class="size-8 text-white"  onPlay={onPlay} onPause={onPause}/>
 			</div>
 		</div>
 		<footer class="flex grow-0 flex-row place-content-center justify-between">
@@ -37,7 +64,9 @@
 				<div class="relative top-3 grid grid-rows-2 gap-0 leading-6">
 					<span>4</span><span>4</span>
 				</div>
-				<TempoButton bind:tempo />
+				{#if metronome !== null}
+				<TempoButton bind:tempo={metronome.tempo} />
+				{/if}
 			</div>
 		</footer>
 	</div>
