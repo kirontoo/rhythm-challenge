a8c3702c764fd87faf6331c4f31d50855f0fb218
Amy Dang <ndangamy@gmail.com>
2025-03-03 16:51:05 -0800

    add type check

--- src/lib/NoteIcons.ts
+++ src/lib/NoteIcons.ts
@@ -3,8 +3,9 @@ import SixteenthNoteIconSVG from "./components/icons/notes/SixteenthNoteIcon.svg
 import EighthNoteIconSVG from "./components/icons/notes/EighthNoteIcon.svg";
 import TripletNoteIconSVG from "./components/icons/notes/TripletNoteIcon.svg";
 import QuarterRestIconSVG from "./components/icons/notes/QuarterRestIcon.svg";
+import { type SVGElement } from "./CanvasRenderer";
 
-const NoteIcons = {
+const NoteIcons: Record<string, SVGElement> = {
     quarter: { src: QuarterNoteIconSVG, width: 100, height: 100 },
     sixteenth: { src: SixteenthNoteIconSVG, width: 100, height: 100 },
     triplet: { src: TripletNoteIconSVG, width: 100, height: 100 },
