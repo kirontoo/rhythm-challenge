21faad44b7b0d514c142493f62f1388c1ffcb3b5
Amy Dang <ndangamy@gmail.com>
2025-03-03 16:42:41 -0800

    add note svgs

---
+++ src/lib/NoteIcons.ts
@@ -0,0 +1,15 @@
+import QuarterNoteIconSVG from "./components/icons/notes/QuarterNoteIcon.svg";
+import SixteenthNoteIconSVG from "./components/icons/notes/SixteenthNoteIcon.svg";
+import EighthNoteIconSVG from "./components/icons/notes/EighthNoteIcon.svg";
+import TripletNoteIconSVG from "./components/icons/notes/TripletNoteIcon.svg";
+import QuarterRestIconSVG from "./components/icons/notes/QuarterRestIcon.svg";
+
+const NoteIcons = {
+    quarter: { src: QuarterNoteIconSVG, width: 100, height: 100 },
+    sixteenth: { src: SixteenthNoteIconSVG, width: 100, height: 100 },
+    triplet: { src: TripletNoteIconSVG, width: 100, height: 100 },
+    eigth: { src: EighthNoteIconSVG, width: 100, height: 100 },
+    quarterRest: { src: QuarterRestIconSVG, width: 100, height: 100 },
+};
+
+export default NoteIcons;
