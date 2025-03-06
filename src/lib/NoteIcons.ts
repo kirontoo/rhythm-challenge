2ef673a9897705d11ec5430f58d76b6dc07314a2
Amy Dang <ndangamy@gmail.com>
2025-03-05 16:19:03 -0800

    add interface BeatTile

--- src/lib/NoteIcons.ts
+++ src/lib/NoteIcons.ts
@@ -5,12 +5,17 @@ import TripletNoteIconSVG from "./components/icons/notes/TripletNoteIcon.svg";
 import QuarterRestIconSVG from "./components/icons/notes/QuarterRestIcon.svg";
 import { type SVGElement } from "./CanvasRenderer";
 
-const NoteIcons: Record<string, SVGElement> = {
-    quarter: { src: QuarterNoteIconSVG, width: 100, height: 100 },
-    sixteenth: { src: SixteenthNoteIconSVG, width: 100, height: 100 },
-    triplet: { src: TripletNoteIconSVG, width: 100, height: 100 },
-    eighth: { src: EighthNoteIconSVG, width: 100, height: 100 },
-    quarterRest: { src: QuarterRestIconSVG, width: 100, height: 100 },
+interface BeatTile extends SVGElement {
+    // tile background color
+    color: string
+}
+
+const NoteIcons: Record<string, BeatTile> = {
+    quarter: { src: QuarterNoteIconSVG, width: 100, height: 100, color: "oklch(0.704 0.191 22.216)" },
+    sixteenth: { src: SixteenthNoteIconSVG, width: 100, height: 100, color: "oklch(0.707 0.165 254.624)" },
+    triplet: { src: TripletNoteIconSVG, width: 100, height: 100, color: "oklch(0.792 0.209 151.711)" },
+    eighth: { src: EighthNoteIconSVG, width: 100, height: 100, color: "oklch(0.828 0.189 84.429)" },
+    quarterRest: { src: QuarterRestIconSVG, width: 100, height: 100, color: "white" },
 };
 
 export default NoteIcons;
