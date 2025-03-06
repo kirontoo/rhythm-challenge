fb98accc2a185952ae87d5f9732b1532ccb662fb
Amy Dang <ndangamy@gmail.com>
2025-03-05 16:19:46 -0800

    rename

--- src/lib/NoteTile.ts
+++ src/lib/NoteTile.ts
@@ -10,7 +10,7 @@ interface BeatTile extends SVGElement {
     color: string
 }
 
-const NoteIcons: Record<string, BeatTile> = {
+const NoteTile: Record<string, BeatTile> = {
     quarter: { src: QuarterNoteIconSVG, width: 100, height: 100, color: "oklch(0.704 0.191 22.216)" },
     sixteenth: { src: SixteenthNoteIconSVG, width: 100, height: 100, color: "oklch(0.707 0.165 254.624)" },
     triplet: { src: TripletNoteIconSVG, width: 100, height: 100, color: "oklch(0.792 0.209 151.711)" },
@@ -18,4 +18,4 @@ const NoteIcons: Record<string, BeatTile> = {
     quarterRest: { src: QuarterRestIconSVG, width: 100, height: 100, color: "white" },
 };
 
-export default NoteIcons;
+export default NoteTile;
